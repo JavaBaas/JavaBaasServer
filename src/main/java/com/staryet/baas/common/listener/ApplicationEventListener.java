4a383ecd364c6e926d3b245880cdaf076586e1f2
2884686@qq.com <2884686@qq.com>
2016-02-28 00:02:36 +0800

    Add mongoDb and redis health check on startup.

--- src/main/java/com/staryet/baas/common/listener/ApplicationEventListener.java
+++ src/main/java/com/staryet/baas/common/listener/ApplicationEventListener.java
@@ -2,8 +2,13 @@ package com.staryet.baas.common.listener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisConnectionUtils;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,14 +18,45 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationEventListener implements ApplicationListener<ApplicationReadyEvent> {
 
-    public static boolean ready;
+    public static boolean error;
+    private static boolean ready;
     private Log log = LogFactory.getLog(getClass());
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        //系统启动成功
         ready = true;
-        log.info("JavaBaasServer started.");
+        //check out database health
+        try {
+            RedisConnection connection = RedisConnectionUtils.getConnection(this.redisConnectionFactory);
+            try {
+                connection.info();
+            } catch (Exception e) {
+                error = true;
+                log.error(e, e);
+            } finally {
+                RedisConnectionUtils.releaseConnection(connection, this.redisConnectionFactory);
+            }
+        } catch (Exception e) {
+            error = true;
+            log.error(e, e);
+        }
+        try {
+            this.mongoTemplate.executeCommand("{ buildInfo: 1 }");
+        } catch (Exception e) {
+            error = true;
+            log.error(e, e);
+        }
+        if (!error) {
+            log.info("JavaBaasServer started.");
+        } else {
+            log.error("JavaBaasServer failed to start!");
+        }
     }
 
     public static boolean isReady() {
