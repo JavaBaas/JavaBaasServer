88f4a121c448000076d02acabd65f4567064013e
2884686@qq.com <2884686@qq.com>
2016-03-03 11:09:47 +0800

    Add Access-Control-Allow-Credentials.

--- src/main/java/com/staryet/baas/common/filter/SimpleCORSFilter.java
+++ src/main/java/com/staryet/baas/common/filter/SimpleCORSFilter.java
@@ -15,6 +15,7 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "content-type,JB-Plat,JB-AppId,JB-Sign,JB-AdminSign,JB-MasterSign,JB-Timestamp");
         chain.doFilter(req, res);
