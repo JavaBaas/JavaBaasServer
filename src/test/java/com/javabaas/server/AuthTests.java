c87cc8465bbd4adecbb514c0d366ff4c39881a35
2884686@qq.com <2884686@qq.com>
2017-06-22 19:21:20 +0800

    增加防重放攻击

--- src/test/java/com/javabaas/server/AuthTests.java
+++ src/test/java/com/javabaas/server/AuthTests.java
@@ -7,47 +7,49 @@ import com.javabaas.server.admin.service.ClazzService;
 import com.javabaas.server.common.entity.SimpleCode;
 import com.javabaas.server.config.AuthConfig;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RootUriTemplateHandler;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.support.HttpRequestWrapper;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Collections;
 import java.util.Date;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by Staryet on 15/9/22.
+ * 权限相关测试
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Main.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = Main.class)
 public class AuthTests {
 
+    private MockMvc mockMvc;
     private App app;
+
     @Autowired
     private AppService appService;
     @Autowired
     private ClazzService clazzService;
     @Autowired
     private AuthConfig authConfig;
+
     @Autowired
-    private RestTemplate restTemplate;
-    @LocalServerPort
-    private String port;
+    private WebApplicationContext webApplicationContext;
 
     @Before
-    public void before() {
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         app = new App();
         app.setName("AuthTestApp");
         appService.insert(app);
@@ -62,141 +64,138 @@ public class AuthTests {
     }
 
     @Test
-    public void testAdmin() {
-        //测试无权限时拒绝访问
-        testNoAdminAuthUrl("/api/admin/app");
-        testNoMasterAuthUrl("/api/master/clazz");
-        testNoMasterAuthUrl("/api/master/clazz/Book/field");
-        testNoUserAuthUrl("/api/object/Book");
-        //测试超级权限
-        RestTemplate rest = getAdminRest();
-        rest.getForObject("/api/admin/app", String.class);
-        //测试管理权限
-        rest = getMasterRest(app);
-        rest.getForObject("/api/master/clazz/Book", String.class);
-        //测试普通权限
-        rest = getUserRest(app);
-        rest.getForObject("/api/object/Book", String.class);
+    public void testNoAdminAuthUrl() throws Exception {
+        mockMvc.perform(defaultRequest("/api/admin/app"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is(SimpleCode.AUTH_NEED_ADMIN_SIGN.getCode())));
+    }
+
+    @Test
+    public void testNoMasterAuthUrl() throws Exception {
+        mockMvc.perform(userRequest("/api/master/clazz"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is(SimpleCode.AUTH_NEED_MASTER_SIGN.getCode())));
+        mockMvc.perform(userRequest("/api/master/clazz/Book/field"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is(SimpleCode.AUTH_NEED_MASTER_SIGN.getCode())));
+    }
+
+    @Test
+    public void testNoUserAuthUrl() throws Exception {
+        mockMvc.perform(defaultRequest("/api/object/Book"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is(SimpleCode.AUTH_LESS.getCode())));
+    }
+
+    @Test
+    public void testAdminAuth() throws Exception {
+        mockMvc.perform(adminRequest("/api/admin/app"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+    @Test
+    public void testMasterAuth() throws Exception {
+        mockMvc.perform(masterRequest("/api/master/clazz/Book"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+    @Test
+    public void testUserAuth() throws Exception {
+        mockMvc.perform(userRequest("/api/object/Book"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+    @Test
+    /**
+     * 测试拒绝重放攻击
+     */
+    public void testReplayAttack() throws Exception {
+        long timestamp = new Date().getTime();
+        String timestampStr = String.valueOf(timestamp);
+        String sign = getSign(app.getKey(), timestampStr);
+        String url = "/api/object/Book";
+        mockMvc.perform(get(url).header("JB-Plat", "cloud")
+                .header("JB-Timestamp", timestampStr)
+                .header("JB-AppId", app.getId())
+                .header("JB-Sign", sign)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+        //重放攻击请求被拒绝
+        mockMvc.perform(get(url).header("JB-Plat", "cloud")
+                .header("JB-Timestamp", timestampStr)
+                .header("JB-AppId", app.getId())
+                .header("JB-Sign", sign)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.code", is(SimpleCode.AUTH_REPLAY_ATTACK.getCode())));
+    }
+
+    private MockHttpServletRequestBuilder defaultRequest(String url) throws Exception {
+        return get(url).header("JB-Plat", "cloud").contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON_UTF8);
     }
 
     /**
-     * 缺少超级权限
+     * 管理权限请求
      *
-     * @param url
+     * @param url 地址
      */
-    private void testNoAdminAuthUrl(String url) {
-        RestTemplate rest = getDefaultRest();
-        try {
-            rest.getForObject(url, String.class);
-            Assert.fail();
-        } catch (HttpClientErrorException exception) {
-            String response = exception.getResponseBodyAsString();
-            Assert.assertThat(response.contains(String.valueOf(SimpleCode.AUTH_NEED_ADMIN_SIGN.getCode())), equalTo(true));
-        }
+    private MockHttpServletRequestBuilder masterRequest(String url) throws Exception {
+        long timestamp = new Date().getTime();
+        String timestampStr = String.valueOf(timestamp);
+        return get(url).header("JB-Plat", "cloud")
+                .header("JB-Timestamp", timestampStr)
+                .header("JB-AppId", app.getId())
+                .header("JB-MasterSign", getSign(app.getMasterKey(), timestampStr))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8);
     }
 
     /**
-     * 缺少管理权限
+     * 超级权限请求
      *
-     * @param url
+     * @param url 地址
      */
-    private void testNoMasterAuthUrl(String url) {
-        RestTemplate rest = getUserRest(app);
-        try {
-            rest.getForObject(url, String.class);
-            Assert.fail();
-        } catch (HttpClientErrorException exception) {
-            String response = exception.getResponseBodyAsString();
-            Assert.assertThat(response.contains(String.valueOf(SimpleCode.AUTH_NEED_MASTER_SIGN.getCode())), equalTo(true));
-        }
+    private MockHttpServletRequestBuilder adminRequest(String url) throws Exception {
+        long timestamp = new Date().getTime();
+        String timestampStr = String.valueOf(timestamp);
+        return get(url).header("JB-Plat", "cloud")
+                .header("JB-Timestamp", timestampStr)
+                .header("JB-AppId", app.getId())
+                .header("JB-AdminSign", getSign(authConfig.getKey(), timestampStr))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8);
     }
 
     /**
-     * 缺少普通权限
+     * 用户级别请求
      *
-     * @param url
+     * @param url 地址
      */
-    private void testNoUserAuthUrl(String url) {
-        RestTemplate rest = getDefaultRest();
-        try {
-            rest.getForObject(url, String.class);
-            Assert.fail();
-        } catch (HttpClientErrorException exception) {
-            String response = exception.getResponseBodyAsString();
-            Assert.assertThat(response.contains(String.valueOf(SimpleCode.AUTH_LESS.getCode())), equalTo(true));
-        }
+    private MockHttpServletRequestBuilder userRequest(String url) throws Exception {
+        long timestamp = new Date().getTime();
+        String timestampStr = String.valueOf(timestamp);
+        return get(url).header("JB-Plat", "cloud")
+                .header("JB-Timestamp", timestampStr)
+                .header("JB-AppId", app.getId())
+                .header("JB-Sign", getSign(app.getKey(), timestampStr))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8);
     }
 
-    public String getSign(String key, String timestamp) {
+    private String getSign(String key, String timestamp) {
         return DigestUtils.md5DigestAsHex((key + ":" + timestamp).getBytes());
-    }
-
-    public RestTemplate getDefaultRest() {
-        ClientHttpRequestInterceptor i = (httpRequest, bytes, clientHttpRequestExecution) -> {
-            HttpRequestWrapper requestWrapper = new HttpRequestWrapper(httpRequest);
-            long timestamp = new Date().getTime();
-            requestWrapper.getHeaders().remove("Content-Type");
-            requestWrapper.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
-            requestWrapper.getHeaders().add("JB-Plat", "cloud");
-            return clientHttpRequestExecution.execute(httpRequest, bytes);
-        };
-        getRest().setInterceptors(Collections.singletonList(i));
-        return restTemplate;
-    }
-
-    public RestTemplate getRest() {
-        restTemplate.setUriTemplateHandler(new RootUriTemplateHandler("http://127.0.0.1:" + port));
-        return restTemplate;
-    }
-
-    public RestTemplate getAdminRest() {
-        ClientHttpRequestInterceptor i = (httpRequest, bytes, clientHttpRequestExecution) -> {
-            HttpRequestWrapper requestWrapper = new HttpRequestWrapper(httpRequest);
-            long timestamp = new Date().getTime();
-            String timestampStr = String.valueOf(timestamp);
-            requestWrapper.getHeaders().remove("Content-Type");
-            requestWrapper.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
-            requestWrapper.getHeaders().add("JB-Timestamp", timestampStr);
-            requestWrapper.getHeaders().add("JB-AdminSign", getSign(authConfig.getKey(), timestampStr));
-            requestWrapper.getHeaders().add("JB-Plat", "cloud");
-            return clientHttpRequestExecution.execute(httpRequest, bytes);
-        };
-        getRest().setInterceptors(Collections.singletonList(i));
-        return restTemplate;
-    }
-
-    public RestTemplate getMasterRest(App app) {
-        ClientHttpRequestInterceptor i = (httpRequest, bytes, clientHttpRequestExecution) -> {
-            HttpRequestWrapper requestWrapper = new HttpRequestWrapper(httpRequest);
-            long timestamp = new Date().getTime();
-            String timestampStr = String.valueOf(timestamp);
-            requestWrapper.getHeaders().remove("Content-Type");
-            requestWrapper.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
-            requestWrapper.getHeaders().add("JB-Timestamp", timestampStr);
-            requestWrapper.getHeaders().add("JB-AppId", app.getId());
-            requestWrapper.getHeaders().add("JB-Plat", "cloud");
-            requestWrapper.getHeaders().add("JB-MasterSign", getSign(app.getMasterKey(), timestampStr));
-            return clientHttpRequestExecution.execute(httpRequest, bytes);
-        };
-        getRest().setInterceptors(Collections.singletonList(i));
-        return restTemplate;
-    }
-
-    public RestTemplate getUserRest(App app) {
-        ClientHttpRequestInterceptor i = (httpRequest, bytes, clientHttpRequestExecution) -> {
-            HttpRequestWrapper requestWrapper = new HttpRequestWrapper(httpRequest);
-            long timestamp = new Date().getTime();
-            String timestampStr = String.valueOf(timestamp);
-            requestWrapper.getHeaders().remove("Content-Type");
-            requestWrapper.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
-            requestWrapper.getHeaders().add("JB-Timestamp", timestampStr);
-            requestWrapper.getHeaders().add("JB-AppId", app.getId());
-            requestWrapper.getHeaders().add("JB-Plat", "cloud");
-            requestWrapper.getHeaders().add("JB-Sign", getSign(app.getKey(), timestampStr));
-            return clientHttpRequestExecution.execute(httpRequest, bytes);
-        };
-        getRest().setInterceptors(Collections.singletonList(i));
-        return restTemplate;
     }
 
 }
