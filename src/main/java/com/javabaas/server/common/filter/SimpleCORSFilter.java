607b85e4d68e265560fc418efc1bc89b48120cfb
2884686@qq.com <2884686@qq.com>
2017-07-18 16:13:42 +0800

    允许所有跨域访问

--- src/main/java/com/javabaas/server/common/filter/SimpleCORSFilter.java
+++ src/main/java/com/javabaas/server/common/filter/SimpleCORSFilter.java
@@ -14,7 +14,7 @@ public class SimpleCORSFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8081");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "content-type,JB-Plat,JB-AppId,JB-Sign,JB-AdminSign,JB-MasterSign,JB-Timestamp");
