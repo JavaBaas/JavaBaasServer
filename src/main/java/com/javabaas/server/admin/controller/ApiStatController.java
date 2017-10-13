fc01eef55cd189a4475efc3a5ac48a419ffae974
zangyilin <112408136qq@gmail.com>
2017-10-13 16:09:03 +0800

    修改ApiStat返回格式

--- src/main/java/com/javabaas/server/admin/controller/ApiStatController.java
+++ src/main/java/com/javabaas/server/admin/controller/ApiStatController.java
@@ -3,6 +3,7 @@ package com.javabaas.server.admin.controller;
 import com.javabaas.server.admin.entity.ApiMethod;
 import com.javabaas.server.admin.entity.ClientPlatform;
 import com.javabaas.server.admin.service.StatService;
+import com.javabaas.server.common.entity.SimpleResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,15 +22,18 @@ public class ApiStatController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
-    public List<Long> getStat(@RequestHeader(value = "JB-AppId") String appId,
-                              @RequestParam(required = false) String method,
-                              @RequestParam(required = false) String clazz,
-                              @RequestParam(required = false) String plat,
-                              @RequestParam String from,
-                              @RequestParam String to) {
+    public SimpleResult getStat(@RequestHeader(value = "JB-AppId") String appId,
+                                @RequestParam(required = false) String method,
+                                @RequestParam(required = false) String clazz,
+                                @RequestParam(required = false) String plat,
+                                @RequestParam String from,
+                                @RequestParam String to) {
         ApiMethod apiMethod = ApiMethod.get(method);
         ClientPlatform clientPlatform = ClientPlatform.get(plat);
-        return statService.get(appId, apiMethod, clazz, clientPlatform, from, to);
+        List<Long> list = statService.get(appId, apiMethod, clazz, clientPlatform, from, to);
+        SimpleResult result = SimpleResult.success();
+        result.putData("result", list);
+        return result;
     }
 
 }
