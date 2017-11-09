60603dd4631e2aa2141e3b9c76f729214d92af0a
Codi <2884686@qq.com>
2017-11-09 22:36:22 +0800

    删除content-type强制检查

--- src/main/java/com/javabaas/server/common/interceptor/HeaderInterceptor.java
+++ src/main/java/com/javabaas/server/common/interceptor/HeaderInterceptor.java
@@ -19,16 +19,9 @@ public class HeaderInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String contentType = request.getHeader("Content-Type");
         if (request.getMethod().equals("OPTIONS")) {
             //遇到OPTIONS请求时直接返回成功
             response.setStatus(200);
-            return false;
-        }
-        if (StringUtils.isEmpty(contentType) || !contentType.contains("application/json")) {
-            //检查contentType
-            response.setStatus(400);
-            response.getOutputStream().write("ContentType must be application/json".getBytes());
             return false;
         }
         if (!checkPlatform(request.getHeader("JB-Plat"))) {
